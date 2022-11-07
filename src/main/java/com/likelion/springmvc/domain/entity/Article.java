186ce3b746d43769fb281b48904dc656f870d3b3
menuHwang <hmin0206@naver.com>
2022-11-07 14:56:21 +0900

    jpa 세팅 및 article 도메인 생성

---
+++ src/main/java/com/likelion/springmvc/domain/entity/Article.java
@@ -0,0 +1,18 @@
+package com.likelion.springmvc.domain.entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+}
