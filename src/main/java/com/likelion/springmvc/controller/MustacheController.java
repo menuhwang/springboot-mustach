93dffb81a0ce514235476ad6c22be85ff79c1f44
menuHwang <hmin0206@naver.com>
2022-11-07 11:43:59 +0900

    greeting 추가

---
+++ src/main/java/com/likelion/springmvc/controller/MustacheController.java
@@ -0,0 +1,15 @@
+package com.likelion.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+    @GetMapping(value = "/hi/{id}")
+    public String mustacheCon(Model model, @PathVariable("id") String id) {
+        model.addAttribute("username", id);
+        return "greetings";
+    }
+}
