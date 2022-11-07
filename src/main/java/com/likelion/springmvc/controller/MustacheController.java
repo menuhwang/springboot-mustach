3694a5b504d2bd091c5dd5603f319bea15d48eef
menuHwang <hmin0206@naver.com>
2022-11-07 13:23:39 +0900

    홈페이지 추가

--- src/main/java/com/likelion/springmvc/controller/MustacheController.java
+++ src/main/java/com/likelion/springmvc/controller/MustacheController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MustacheController {
+    @GetMapping("")
+    public String home() {
+        return "index";
+    }
     @GetMapping(value = "/hi/{id}")
     public String mustacheCon(Model model, @PathVariable("id") String id) {
         model.addAttribute("username", id);
